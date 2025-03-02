f0adf8ee23aac31a4e70bf830a48936ea416a8a2
SShivani13 <shivanisingh072000@gmail.com>
2025-03-02 23:47:11 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,21 @@
 
+#Creating and Running a Python Script for CPU Utilization Test
+mkdir test1
+cd test1
+v1 test.py
+
+
+#Infinite Number Generator Using Python Yield
+def to_infinity():
+    index = 0
+    while True:
+        yield index
+        index += 1
+
+for i in to_infinity():
+    if i > 10:
+        i1=1
+
+# Press :x and enter will open cmd line 
+# run the below commond to check the utilization of CPU 
+python3 test.py
